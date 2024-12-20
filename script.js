e81a3a824cb46468aef8a68f4aaeac5509af3f8f
OmMehta07 <64518918+ommehta4920@users.noreply.github.com>
2024-12-20 18:57:55 +0530

    Small changes
    Favicon, resume, linkedin and github links added

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
+// Load from Hero Section on Refresh
+
+// Smooth scroll to hero section on reload
+window.onload = function () {
+  document.getElementById("#profile").scrollIntoView({ behavior: "smooth", block: "start" });
+};
+
+
+// Hemburger Menu
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// TypeWriter Effect
+
+const texts = [
+  "Aspiring Data Analyst",
+  "Software Developer",
+  "Python Enthusiast!"
+];
+const typingElement = document.getElementById("typing");
+
+let textIndex = 0; // Index of the current text in the array
+let charIndex = 0; // Current character position in the text
+let isDeleting = false;
+
+function typeText() {
+  const currentText = texts[textIndex];
+  
+  if (!isDeleting) {
+      typingElement.innerHTML = currentText.substring(0, charIndex);
+      charIndex++;
+
+      if (charIndex > currentText.length) {
+          isDeleting = true; // Start deleting after full text is typed
+          setTimeout(typeText, 1000); // Pause before deleting
+          return;
+      }
+  } else {
+      typingElement.innerHTML = currentText.substring(0, charIndex);
+      charIndex--;
+
+      if (charIndex < 0) {
+          isDeleting = false; // Move to the next text
+          textIndex = (textIndex + 1) % texts.length; // Loop back to first text
+      }
+  }
+
+  const speed = isDeleting ? 50 : 150;
+  setTimeout(typeText, speed);
+}
+window.onload = typeText;
+
+
 
 // Dark / light mode
 
